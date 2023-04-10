170c02abdf4bae07000fb2d6a82d1bc47c30473d
Sankaku <sankaku_dlt.45631@outlook.jp>
2023-04-10 12:52:01 +0000

    Initial commit

---
+++ lib/deep_sinker.ex
@@ -0,0 +1,18 @@
+defmodule DeepSinker do
+  @moduledoc """
+  Documentation for `DeepSinker`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DeepSinker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/deep_sinker_test.exs
@@ -0,0 +1,8 @@
+defmodule DeepSinkerTest do
+  use ExUnit.Case
+  doctest DeepSinker
+
+  test "greets the world" do
+    assert DeepSinker.hello() == :world
+  end
+end
