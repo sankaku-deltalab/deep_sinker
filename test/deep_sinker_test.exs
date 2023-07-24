6fd8540cc00e28d1d2f9480a1d6e076d87ccd238
Sankaku <sankaku_dlt.45631@outlook.jp>
2023-07-24 08:59:17 +0000

    Refactor test

--- test/deep_sinker_test.exs
+++ test/deep_sinker_test.exs
@@ -7,7 +7,7 @@ defmodule DeepSinkerTest do
     |> Enum.map(fn {path, marker} -> {Path.join(root, path), marker} end)
   end
 
-  defp with_tmp_files(callback) do
+  defp tmp_files_fixture(root) do
     # - root
     #   - dir_1
     #     - dir_1_1
@@ -20,126 +20,126 @@ defmodule DeepSinkerTest do
     #     - file_3_1.txt
     #   - file_1.txt
     #   - file_2.txt
-    Tmp.dir(fn root ->
-      rel_dirs = ["dir_1/dir_1_1", "dir_1/dir_1_2", "dir_2/dir_2_1", "dir_3"]
+    rel_dirs = ["dir_1/dir_1_1", "dir_1/dir_1_2", "dir_2/dir_2_1", "dir_3"]
 
-      for rel_dir <- rel_dirs do
-        File.mkdir_p!(Path.join(root, rel_dir))
-      end
+    for rel_dir <- rel_dirs do
+      File.mkdir_p!(Path.join(root, rel_dir))
+    end
 
-      rel_files = [
-        "dir_1/dir_1_1/file_1_1_1.txt",
-        "dir_1/file_1_1.txt",
-        "dir_3/file_3_1.txt",
-        "file_1.txt",
-        "file_2.txt"
+    rel_files = [
+      "dir_1/dir_1_1/file_1_1_1.txt",
+      "dir_1/file_1_1.txt",
+      "dir_3/file_3_1.txt",
+      "file_1.txt",
+      "file_2.txt"
+    ]
+
+    for rel_file <- rel_files do
+      File.write(Path.join(root, rel_file), "")
+    end
+  end
+
+  @tag :tmp_dir
+  test "iter default", %{tmp_dir: root} do
+    tmp_files_fixture(root)
+
+    found_items =
+      DeepSinker.new([{root, :a}])
+      |> DeepSinker.stream()
+      |> Enum.to_list()
+
+    expected_items =
+      [
+        {"dir_1/dir_1_1/file_1_1_1.txt", :a},
+        {"dir_1/file_1_1.txt", :a},
+        {"dir_3/file_3_1.txt", :a},
+        {"file_1.txt", :a},
+        {"file_2.txt", :a}
       ]
+      |> join_root_to_items(root)
 
-      for rel_file <- rel_files do
-        File.write(Path.join(root, rel_file), "")
-      end
-
-      callback.(root)
-    end)
+    assert found_items == expected_items
   end
 
-  test "iter default" do
-    with_tmp_files(fn root ->
-      found_items =
-        DeepSinker.new([{root, :a}])
-        |> DeepSinker.stream()
-        |> Enum.to_list()
+  @tag :tmp_dir
+  test "iter with multiple roots", %{tmp_dir: root} do
+    tmp_files_fixture(root)
 
-      expected_items =
-        [
-          {"dir_1/dir_1_1/file_1_1_1.txt", :a},
-          {"dir_1/file_1_1.txt", :a},
-          {"dir_3/file_3_1.txt", :a},
-          {"file_1.txt", :a},
-          {"file_2.txt", :a}
-        ]
-        |> join_root_to_items(root)
+    roots =
+      [{"dir_1", :a}, {"dir_3", :b}]
+      |> join_root_to_items(root)
 
-      assert found_items == expected_items
-    end)
+    filepaths =
+      DeepSinker.new(roots)
+      |> DeepSinker.stream()
+      |> Enum.to_list()
+
+    expected_filepaths =
+      [
+        {"dir_1/dir_1_1/file_1_1_1.txt", :a},
+        {"dir_1/file_1_1.txt", :a},
+        {"dir_3/file_3_1.txt", :b}
+        # "file_1.txt",
+        # "file_2.txt"
+      ]
+      |> join_root_to_items(root)
+
+    assert filepaths == expected_filepaths
   end
 
-  test "iter with multiple roots" do
-    with_tmp_files(fn root ->
-      roots =
-        [{"dir_1", :a}, {"dir_3", :b}]
-        |> join_root_to_items(root)
+  @tag :tmp_dir
+  test "iter desc", %{tmp_dir: root} do
+    tmp_files_fixture(root)
 
-      filepaths =
-        DeepSinker.new(roots)
-        |> DeepSinker.stream()
-        |> Enum.to_list()
+    filepaths =
+      DeepSinker.new([{root, :a}], order: :desc)
+      |> DeepSinker.stream()
+      |> Enum.to_list()
 
-      expected_filepaths =
-        [
-          {"dir_1/dir_1_1/file_1_1_1.txt", :a},
-          {"dir_1/file_1_1.txt", :a},
-          {"dir_3/file_3_1.txt", :b}
-          # "file_1.txt",
-          # "file_2.txt"
-        ]
-        |> join_root_to_items(root)
+    expected_filepaths =
+      [
+        {"dir_1/dir_1_1/file_1_1_1.txt", :a},
+        {"dir_1/file_1_1.txt", :a},
+        {"dir_3/file_3_1.txt", :a},
+        {"file_1.txt", :a},
+        {"file_2.txt", :a}
+      ]
+      |> join_root_to_items(root)
+      |> Enum.reverse()
 
-      assert filepaths == expected_filepaths
-    end)
+    assert filepaths == expected_filepaths
   end
 
-  test "iter desc" do
-    with_tmp_files(fn root ->
-      filepaths =
-        DeepSinker.new([{root, :a}], order: :desc)
-        |> DeepSinker.stream()
-        |> Enum.to_list()
+  @tag :tmp_dir
+  test "iter with handler", %{tmp_dir: root} do
+    tmp_files_fixture(root)
 
-      expected_filepaths =
-        [
-          {"dir_1/dir_1_1/file_1_1_1.txt", :a},
-          {"dir_1/file_1_1.txt", :a},
-          {"dir_3/file_3_1.txt", :a},
-          {"file_1.txt", :a},
-          {"file_2.txt", :a}
-        ]
-        |> join_root_to_items(root)
-        |> Enum.reverse()
+    filepaths =
+      DeepSinker.new([{root, :a}])
+      |> DeepSinker.stream(
+        handler: fn {path, :a} ->
+          basename = Path.basename(path)
 
-      assert filepaths == expected_filepaths
-    end)
-  end
-
-  test "iter with handler" do
-    with_tmp_files(fn root ->
-      filepaths =
-        DeepSinker.new([{root, :a}])
-        |> DeepSinker.stream(
-          handler: fn {path, :a} ->
-            basename = Path.basename(path)
-
-            # Check is_dir without Path lib.
-            cond do
-              basename == "file_1_1_1.txt" -> :ignore
-              String.ends_with?(basename, ".txt") -> :file
-              true -> :directory
-            end
+          # Check is_dir without Path lib.
+          cond do
+            basename == "file_1_1_1.txt" -> :ignore
+            String.ends_with?(basename, ".txt") -> :file
+            true -> :directory
           end
-        )
-        |> Enum.to_list()
+        end
+      )
+      |> Enum.to_list()
 
-      expected_filepaths =
-        [
-          # "dir_1/dir_1_1/file_1_1_1.txt",
-          {"dir_1/file_1_1.txt", :a},
-          {"dir_3/file_3_1.txt", :a},
-          {"file_1.txt", :a},
-          {"file_2.txt", :a}
-        ]
-        |> join_root_to_items(root)
+    expected_filepaths =
+      [
+        # "dir_1/dir_1_1/file_1_1_1.txt",
+        {"dir_1/file_1_1.txt", :a},
+        {"dir_3/file_3_1.txt", :a},
+        {"file_1.txt", :a},
+        {"file_2.txt", :a}
+      ]
+      |> join_root_to_items(root)
 
-      assert filepaths == expected_filepaths
-    end)
+    assert filepaths == expected_filepaths
   end
 end
